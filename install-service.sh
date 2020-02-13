174ce04bd3526414220dd5c3fda673e303a1e0be
Peter Schuster <development@peschuster.de>
2020-02-13 22:06:20 +0100

    Modified service installer

--- install-service.sh
+++ install-service.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-SERVICE_PATH=/etc/systemd/system/companion.service
+NAME=companion
+SERVICE_PATH=/etc/systemd/system/${companion}.service
 
 USAGE="sudo ./install-service.sh"
 
 read -r -d '' UNIT_FILE << EOF
 [Unit]
 Description=Bitfocus Companion Service
-After=multi-user.target
+After=network-online.target
+Wants=network-online.target
 
 [Service]
 Type=simple
 WorkingDirectory=$(pwd)
 Environment="COMPANION_CONFIG_BASEDIR=/home/pi"
-ExecStart=node headless.js eth0
+ExecStart=node headless.js 0.0.0.0
+KillSignal=SIGINT
+TimeoutStopSec=60
 Restart=on-failure
 User=pi
 
@@ -23,6 +27,6 @@ EOF
 printf "\nInstalling service to: $SERVICE_PATH\n"
 echo "$UNIT_FILE" > $SERVICE_PATH
 systemctl daemon-reload
-systemctl enable --no-pager companion.service
-systemctl restart --no-pager companion.service
-systemctl status --no-pager companion.service
+systemctl enable --no-pager ${companion}.service
+systemctl restart --no-pager ${companion}.service
+systemctl status --no-pager ${companion}.service
